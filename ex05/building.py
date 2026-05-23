a796599ed02e3087ebc424176456c2b1cd2537df
kaveOO <alban.billiette@gmail.com>
2026-05-23 17:28:12 +0200

    + | ex05 finished

---
+++ ex05/building.py
@@ -0,0 +1,51 @@
+import sys as sy
+
+
+def main():
+    """
+        this is a test documentation
+        i'm here
+        hereeeeeeeeee
+    """
+
+    if len(sy.argv) > 2:
+        print(AssertionError("AssertionError: too much arguments"))
+        sy.exit()
+    elif len(sy.argv) <= 1:
+        try:
+            text = input("What is the text to count?\n")
+        except EOFError:
+            print(AssertionError("AssertionError: EOF error"))
+            sy.exit()
+    else:
+        text = sy.argv[1]
+
+    print(f"The text contains {len(text)} characters:")
+
+    upper = 0
+    lower = 0
+    punctuation = 0
+    spaces = 0
+    digits = 0
+
+    for i in text:
+        if i.isupper():
+            upper += 1
+        elif i.islower():
+            lower += 1
+        elif i in "'.,;:?!":
+            punctuation += 1
+        elif i.isspace():
+            spaces += 1
+        elif i.isdigit():
+            digits += 1
+
+    print(f"{upper} upper letters")
+    print(f"{lower} lower letters")
+    print(f"{punctuation} punctuation marks")
+    print(f"{spaces} spaces")
+    print(f"{digits} digits ")
+
+
+if __name__ == "__main__":
+    main()
