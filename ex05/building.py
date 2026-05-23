b0931b34bb45f690374c2ae402a58a98a256eb54
kaveOO <alban.billiette@gmail.com>
2026-05-23 17:35:27 +0200

    + | ex05 splitted into another function

--- ex05/building.py
+++ ex05/building.py
@@ -1,26 +1,10 @@
 import sys as sy
 
 
-def main():
+def count_and_print(text: any):
     """
-        this is a test documentation
-        i'm here
-        hereeeeeeeeee
+        Counting and printing element of a string
     """
-
-    if len(sy.argv) > 2:
-        print(AssertionError("AssertionError: too much arguments"))
-        sy.exit()
-    elif len(sy.argv) <= 1:
-        try:
-            text = input("What is the text to count?\n")
-        except EOFError:
-            print(AssertionError("AssertionError: EOF error"))
-            sy.exit()
-    else:
-        text = sy.argv[1]
-
-    print(f"The text contains {len(text)} characters:")
 
     upper = 0
     lower = 0
@@ -40,11 +24,32 @@ def main():
         elif i.isdigit():
             digits += 1
 
+    print(f"The text contains {len(text)} characters:")
     print(f"{upper} upper letters")
     print(f"{lower} lower letters")
     print(f"{punctuation} punctuation marks")
     print(f"{spaces} spaces")
     print(f"{digits} digits ")
+
+
+def main():
+    """
+        This is the main function yayyyyyyyyyyyyyyy
+    """
+
+    if len(sy.argv) > 2:
+        print(AssertionError("AssertionError: too much arguments"))
+        sy.exit()
+    elif len(sy.argv) <= 1:
+        try:
+            text = input("What is the text to count?\n")
+        except EOFError:
+            print(AssertionError("AssertionError: EOF error"))
+            sy.exit()
+    else:
+        text = sy.argv[1]
+
+    count_and_print(text)
 
 
 if __name__ == "__main__":
